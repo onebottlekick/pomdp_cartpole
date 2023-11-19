7edb0dcbaeb92cafe36839fbb9451e8e3269a1d6
onebottlekick <gksqudcks97@gmail.com>
2023-11-20 02:24:37 +0900

    .

--- buffer.py
+++ buffer.py
@@ -39,17 +39,18 @@ def make_epi_seq(episode, batch_size, device):
 
 
 class ReplayBuffer:
-    def __init__(self, capacity=10000, seq_len=64):
+    def __init__(self, capacity=10000, seq_len=64, random_sample=True):
         self.memory = deque([], maxlen=capacity)
         self.batch_size = seq_len
+        self.random_sample = random_sample
         
     def push(self, args):
         self.memory.append(Transition(*args))
         
-    def sample(self, batch_size=None, random_sample=True):
+    def sample(self, batch_size=None):
         if batch_size == None:
             batch_size = self.batch_size
-        if random_sample:
+        if self.random_sample:
             return random.sample(self.memory, self.batch_size)
         else:
             return self.memory[-batch_size:]
@@ -59,17 +60,18 @@ class ReplayBuffer:
     
     
 class EpisodeBuffer:
-    def __init__(self, capacity=10000, batch_size=32):
+    def __init__(self, capacity=10000, batch_size=32, random_sample=True):
         self.memory = deque([], maxlen=capacity)
         self.batch_size = batch_size
+        self.random_sample = random_sample
         
     def push(self, episode):
         self.memory.append(episode)
         
-    def sample(self, batch_size=None, random_sample=True):
+    def sample(self, batch_size=None):
         if batch_size == None:
             batch_size = self.batch_size
-        if random_sample:
+        if self.random_sample:
             return random.sample(self.memory, self.batch_size)
         else:
             return self.memory[-batch_size:]
